04ae4d05a5143246f009d9e815cde7bded2a35f3
Virtech <Virtech@DESKTOP-8FMQ1MB>
2020-02-06 21:17:48 +0500

    Создание таблицы

---
+++ StudentsDB/Form6.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentsDB
+{
+    public partial class Form6 : Form
+    {
+        public Form6()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void студентыBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.студентыBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.zakharovDataSet);
+
+        }
+
+        private void студентыBindingNavigatorSaveItem_Click_1(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.студентыBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.zakharovDataSet);
+
+        }
+
+        private void студентыBindingNavigatorSaveItem_Click_2(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.студентыBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.zakharovDataSet);
+
+        }
+
+        private void Form6_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "zakharovDataSet.Студенты". При необходимости она может быть перемещена или удалена.
+            this.студентыTableAdapter.Fill(this.zakharovDataSet.Студенты);
+
+        }
+    }
+}
